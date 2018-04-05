7c5d1f16bb7761fc77705b14f5d3f5c06c44560d
Maxence Delattre <maxencedelattre62@gmail.com>
2018-04-05 17:45:00 +0200

    0.1.0

---
+++ README.md
@@ -0,0 +1,11 @@
+# Etd Form Plugin
+
+The **etd-form plugin** for [Grav](http://github.com/getgrav/grav) adds the ability to create and use special form fields, in addition of the form plugin.
+
+# Configuration
+
+Simply copy the `user/plugins/form/form.yaml` into `user/config/plugins/form.yaml` and make your modifications.
+
+```
+enabled: true
+```  
---
+++ etd-form.php
@@ -0,0 +1,53 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+use RocketTheme\Toolbox\Event\Event;
+
+/**
+ * Class ETDFormPlugin
+ * @package Grav\Plugin
+ */
+class ETDFormPlugin extends Plugin
+{
+    /**
+     * @return array
+     *
+     * The getSubscribedEvents() gives the core a list of events
+     *     that the plugin wants to listen to. The key of each
+     *     array section is the event that the plugin listens to
+     *     and the value (in the form of an array) contains the
+     *     callable (or function) as well as the priority. The
+     *     higher the number the higher the priority.
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0]
+        ];
+    }
+
+    /**
+     * Initialize the plugin
+     */
+    public function onPluginsInitialized()
+    {
+        // Don't proceed if we are in the admin plugin
+        if ($this->isAdmin()) {
+            return;
+        }
+
+        // Enable the main event we are interested in
+        $this->enable([
+            'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0]
+        ]);
+    }
+
+    /**
+     * Add current directory to twig lookup paths.
+     */
+    public function onTwigTemplatePaths()
+    {
+        $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
+    }
+}
